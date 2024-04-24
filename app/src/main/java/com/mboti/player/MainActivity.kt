5cc2331d94fcefd50a5a4f45f1dff1bb37fc57d3
botimounir <botimounir@yahoo.fr>
2024-04-24 16:37:28 +0200

    Lecteur Simple Terminé

--- app/src/main/java/com/mboti/player/MainActivity.kt
+++ app/src/main/java/com/mboti/player/MainActivity.kt
@@ -1,16 +1,50 @@
 package com.mboti.player
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
+import com.mboti.player.Commun.convertToText
 import com.mboti.player.model.Music
 import com.mboti.player.model.playList
 import com.mboti.player.ui.theme.PlayerTheme
+import kotlinx.coroutines.delay
 
 
 /*--------------------------------------------------------
@@ -32,7 +66,6 @@ class MainActivity : ComponentActivity() {
 
         player = ExoPlayer.Builder(this).build()
 
-
         setContent {
             PlayerTheme {
                 Surface(
@@ -45,7 +78,175 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    private fun SongScreen(playList: List<Music>) {
 
+    @Composable
+    private fun SongScreen(playList: List<Music>) {
+        InitProcessus(playList)
+
+    }
+
+
+    @Composable
+    private fun InitProcessus(playList: List<Music>) {
+        val playingSongIndex = remember { mutableIntStateOf(0) }
+
+        // currentMediaItemIndex : Returns the index of the current MediaItem in the timeline,
+        // or the prospective index if the current timeline is empty.
+        LaunchedEffect(player.currentMediaItemIndex) {
+            playingSongIndex.intValue = player.currentMediaItemIndex
+        }
+
+        LaunchedEffect(Unit) {
+            playList.forEach {
+                val path = "android.resource://" + packageName + "/" + it.music
+                val mediaItem = MediaItem.fromUri(Uri.parse(path))
+                player.addMediaItem(mediaItem)
+            }
+        }
+
+        player.prepare()
+
+        val isPlaying = remember { mutableStateOf(false) }
+        val currentPosition = remember { mutableLongStateOf(0) }
+
+        LaunchedEffect(key1 = player.currentPosition, key2 = player.isPlaying) {
+            delay(1000)
+            currentPosition.longValue = player.currentPosition
+        }
+
+
+        val sliderPosition = remember { mutableLongStateOf(0) }
+        LaunchedEffect(currentPosition.longValue) {
+            sliderPosition.longValue = currentPosition.longValue
+        }
+
+
+
+        val totalDuration = remember { mutableLongStateOf(0) }
+        LaunchedEffect(player.duration) {
+            if (player.duration > 0) {
+                totalDuration.longValue = player.duration
+            }
+        }
+
+
+        /**
+         *   UI
+         */
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 32.dp),
+        ) {
+
+            TrackSlider(
+                value = sliderPosition.longValue.toFloat(),
+                onValueChange = {
+                    sliderPosition.longValue = it.toLong()
+                },
+                onValueChangeFinished = {
+                    currentPosition.longValue = sliderPosition.longValue
+                    player.seekTo(sliderPosition.longValue)
+                },
+                songDuration = totalDuration.longValue.toFloat()
+            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+
+                Text(
+                    text = (currentPosition.longValue).convertToText(),
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(8.dp),
+                    color = Color.Black,
+                    style = TextStyle(fontWeight = FontWeight.Bold)
+                )
+
+                val remainTime = totalDuration.longValue - currentPosition.longValue
+                Text(
+                    text = if (remainTime >= 0) remainTime.convertToText() else "",
+                    modifier = Modifier
+                        .padding(8.dp),
+                    color = Color.Black,
+                    style = TextStyle(fontWeight = FontWeight.Bold)
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            ButtonControl(icon = R.drawable.ic_previous, size = 40.dp, onClick = {
+                player.seekToPreviousMediaItem()
+            })
+            Spacer(modifier = Modifier.width(20.dp))
+            ButtonControl(
+                icon = if (isPlaying.value) R.drawable.ic_pause else R.drawable.ic_play,
+                size = 100.dp,
+                onClick = {
+                    if (isPlaying.value) {
+                        player.pause()
+                    } else {
+                        player.play()
+                    }
+                    isPlaying.value = player.isPlaying
+                })
+            Spacer(modifier = Modifier.width(20.dp))
+            ButtonControl(icon = R.drawable.ic_next, size = 40.dp, onClick = {
+                player.seekToNextMediaItem()
+            })
+        }
+    }
+
+
+    /**
+     * Tracks and visualizes the song playing actions.
+     */
+    @Composable
+    fun TrackSlider(
+        value: Float,
+        onValueChange: (newValue: Float) -> Unit,
+        onValueChangeFinished: () -> Unit,
+        songDuration: Float
+    ) {
+        Slider(
+            value = value,
+            onValueChange = {
+                onValueChange(it)
+            },
+            onValueChangeFinished = {
+
+                onValueChangeFinished()
+
+            },
+            valueRange = 0f..songDuration,
+            colors = SliderDefaults.colors(
+                thumbColor = Color.Black,
+                activeTrackColor = Color.DarkGray,
+                inactiveTrackColor = Color.Gray,
+            )
+        )
+    }
+
+    @Composable
+    fun ButtonControl(icon: Int, size: Dp, onClick: () -> Unit) {
+        Box(
+            modifier = Modifier
+                .size(size)
+                .clip(CircleShape)
+                .clickable {
+                    onClick()
+                }, contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                modifier = Modifier.size(size / 1.5f),
+                painter = painterResource(id = icon),
+                tint = Color.Black,
+                contentDescription = null
+            )
+        }
     }
 }
